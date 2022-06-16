c44356eb987cd1f552af507d97c18d63afb3d2a0
Jrt345 <JonTech543@protonmail.com>
2022-06-15 23:14:45 -0700

    Re-create basic unit conversionRouter function in UnitConverter.kt

--- src/main/kotlin/UnitConverter.kt
+++ src/main/kotlin/UnitConverter.kt
@@ -1,18 +1,39 @@
 class UnitConverter {
     companion object {
         fun conversionRouter(arguments: Array<String>) {
-            if (arguments[0].lowercase() == "convt" || arguments[0].lowercase() == "cvt" || arguments[0].lowercase() == "convert") {
-                when(arguments[1].lowercase()) {
-                    "temp","temperature" -> temperatureConverter(arguments[2].toDouble(), arguments[3], arguments[5])
-                    "len","length"-> lengthConverter()
-                    "a","area" -> areaConverter()
-                    "v", "vol", "volume" -> volumeConverter()
-                    "m", "mass" -> massConverter()
+            var fromValue = ""
+
+            for (i in arguments[1]) {
+                if (i.isLetter()){
+                    break
                 }
+
+                fromValue = fromValue.plus(i)
             }
+
+            val fromUnit = arguments[1].drop(fromValue.length)
+
+            val toUnit = arguments[2]
+
+            when (fromValue) {
+                "degf","degc","f","c" -> temperatureConverter()
+
+                "mm","cm","m","km","in","ft","yd","mi" -> lengthConverter()
+
+                "ac","mm2","cm2","m2","ha","km2","in2","ft2","yd2","mi2" -> areaConverter()
+
+                "mm3","ml","l","m3","km3","in3","ft3","yd3","mi3",
+                "gal","qt","pt","cup","cups","floz","tbsp","tsp",
+                "impgal","impqt","imppt","impcup","impcups",
+                "impfloz","imptbsp","imptsp", -> volumeConverter()
+
+                "mg","g","kg","t","lb","lbs","oz","ton","impton","st","impst" -> massConverter()
+            }
+
+
         }
 
-        fun temperatureConverter(value:Double, unitFrom:String, unitTo:String){
+        fun temperatureConverter(){
 
         }
 
