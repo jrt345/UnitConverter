3df026f8c3fc36f2808eefa085bfdc37608e0938
Jrt345 <JonTech543@protonmail.com>
2022-06-16 19:44:54 -0700

    Create CommandRouter.kt to re-route commands to certain functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,7 @@ fun main(){
     println("Welcome to UnitConverter: " +
             "You can convert: temperature, length, area, volume, and mass, using commands.")
     while (true) {
-        val input: String = readln()
-        val typed = input.split(" ").toTypedArray();
-        if (typed.size == 3 && typed[0] == "cvt"){
-            UnitConverter.conversionRouter(typed)
-        } else {
-            println("Proper usage: <cvt> <xu1> <u2>, x = any number, u1 = unit 1, u2 = unit 2")
-        }
+        val inputString: String = readln()
+        CommandRouter.commandRouter(inputString)
     }
 }
