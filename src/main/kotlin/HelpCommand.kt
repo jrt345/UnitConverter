51694d9ed8a6a1f1b1b2a204a1f6d0cfbb9c1120
Jrt345 <JonTech543@protonmail.com>
2022-06-18 11:24:14 -0700

    Create start function in HelpCommand.kt

--- src/main/kotlin/HelpCommand.kt
+++ src/main/kotlin/HelpCommand.kt
@@ -1,3 +1,30 @@
 class HelpCommand {
+    companion object {
+        fun start() {
+            while (true) {
+                println("Type the number of one of the following options to learn more about:\n" +
+                        "[1]cvt command\n" +
+                        "[2]about command'\n" +
+                        "[3]chkupd command\n" +
+                        "[4]Exit help menu")
 
+                val stringInput:String = readln()
+
+                try {
+                    val intInput = stringInput.toInt()
+
+                    if (intInput == 4){
+                        println("Exited help menu, you may type a command now:")
+                        break
+                    } else if (intInput in 1..3) {
+
+                    } else {
+                        println("'$intInput' is out of range, try again")
+                    }
+                } catch (e: NumberFormatException) {
+                    println("'$stringInput' is an invalid option, try again")
+                }
+            }
+        }
+    }
 }
